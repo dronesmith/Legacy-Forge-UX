351c937f0470bf229c903f6c59bb90199315ec86
flatlevel <geoff@skyworksas.com>
2016-02-19 15:49:06 -0800

    Add socket timeout checker to client. Update service so it affects all uses of this dep

--- public/app/services/stream.service.js
+++ public/app/services/stream.service.js
@@ -2,9 +2,25 @@
 
 angular
   .module('ForgeApp')
-  .factory('Stream', function(
-    socketFactory) {
+  .factory('Stream', function(socketFactory) {
+    var mainConnection = io.connect();
+    var connected = true; // connection is true unless session check fails
 
-    return socketFactory();
+    var Stream = socketFactory({
+      ioSocket: mainConnection
+    });
+
+    Stream.on('disconnect', function() {
+      connected = false;
+    });
+
+    setInterval(function() {
+      if (connected == false) {
+        console.log("Attempting reconnect...");
+        Stream.connect();
+      }
+    }, 5000);
+
+    return Stream;
   })
 ;
