8dc0b8cf215702157b70514dee2d82cbb858a74c
flatlevel <geoff@skyworksas.com>
2016-07-06 15:22:31 -0700

    Add legal tender

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,15 @@
+/**
+ * Dronesmith Cloud
+ *
+ * Principle Engineer: Geoff Gardner <geoff@dronesmith.io>
+ *
+ * Copyright (C) 2016 Dronesmith Technologies Inc, all rights reserved.
+ * Unauthorized copying of any source code or assets within this project, via
+ * any medium is strictly prohibited.
+ *
+ * Proprietary and confidential.
+ */
+
 module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-run');
--- public/app/code/code.controller.js
+++ public/app/code/code.controller.js
@@ -1,3 +1,15 @@
+/**
+ * Dronesmith Cloud
+ *
+ * Principle Engineer: Geoff Gardner <geoff@dronesmith.io>
+ *
+ * Copyright (C) 2016 Dronesmith Technologies Inc, all rights reserved.
+ * Unauthorized copying of any source code or assets within this project, via
+ * any medium is strictly prohibited.
+ *
+ * Proprietary and confidential.
+ */
+
 angular
   .module('ForgeApp')
   .controller('CodeCtrl', function ($scope, $http, Stream, User) {
